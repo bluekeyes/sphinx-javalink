2370460d31444e3a5b89a867ebadc17f9f8f7c75
Billy Keyes <bluekeyes@gmail.com>
2015-03-08 17:52:14 -0700

    Update version to 0.9.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ See the `GitHub project`_ for documentation.
 
 setup(
     name='sphinx-javalink',
-    version='0.9.1',
+    version='0.9.2',
 
     description='Link to Javadoc APIs from Sphinx documentation',
     long_description=long_description,
